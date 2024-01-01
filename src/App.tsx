d8ce45e474bc0b13911a19fdd2e55de04f09795a
Yashbstnmnhss <yashbsntmnhss@qq.com>
2024-01-01 11:53:04 +0800

    q

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,10 @@ function App() {
 
     return (
         <>
-            <h1>Vite + Solid</h1>
+            <h1>BSTN 4</h1>
             <div class="card">
                 <button onClick={() => setCount(count => count + 1)}>count is {count()}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
             </div>
-            <p class="read-the-docs">Click on the Vite and Solid logos to learn more</p>
         </>
     )
 }
